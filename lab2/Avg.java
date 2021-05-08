77784efac957db988c090b26e840ab847823bd02
AniruddhaKingaonkar <73694940+AniruddhaKingaonkar@users.noreply.github.com>
2021-05-08 15:25:36 +0530

    Add files via upload

---
+++ lab2/Avg.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Avg
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner (System.in);
+System.out.println("Enter three no:");
+int b=sc.nextInt();
+int c=sc.nextInt();
+int d=sc.nextInt();
+
+int a=(b+c+d)/3;
+System.out.println("Product:"+a);
+
+}
+}
